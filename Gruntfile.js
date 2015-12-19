b542bcb3012600e61aa4eac8f1800817ea27c76f
Iio Yuuki <Yuuki@Iio-no-MacBook-Pro.local>
2015-12-20 00:20:22 +0900

    プロジェクト構成変更によるGruntfile.jsの変更

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,9 @@ module.exports = function (grunt) {
     var exec = require('child_process').exec;
 
     var done = this.async();
-    var command = 'javac MainApp.java; java MainApp; rm -rf *.class';
+    var command = 'javac src/*.java;';
+    command += 'cd src; java MainApp;';
+    command += 'rm -rf *.class';
     var options = { timeout: 120000 };
     var callback = function(error, stdout, stderr) {
       console.log(stdout);
